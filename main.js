f43e33cf29718c1a37fff5a640273f5411d5423a
3Huty <sawczuk.a@gmail.com>
2018-03-14 10:00:41 +0100

    add index.html and main.js

--- main.js
+++ main.js
@@ -5,14 +5,11 @@ var story = document.querySelector('.story');
 var storyText = 'It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty: 0o, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but he was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 
 var insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
-
 var insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
-
 var insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 
-
 function randomValueFromArray(array) {
-  return array[Math.floor(Math.random() * array.length)];
+  return array[Math.floor(Math.random()*array.length)];
 }
 
 randomize.addEventListener('click', result);
@@ -24,11 +21,10 @@ function result() {
   var yItem = randomValueFromArray(insertY);
   var zItem = randomValueFromArray(insertZ);
     
-  var newStory = newStory.replace(/':insertx:'/g, xItem);
-  var newStory = newStory.replace(/':inserty:'/g, yItem);
-  var newStory = newStory.replace(/':insertx:'/g, zItem);
-  var newStory = newStory.replace(/':insertx:'/g, xItem):
-}
+  newStory = newStory.replace(':insertx:', xItem);
+  newStory = newStory.replace(':inserty:', yItem);
+  newStory = newStory.replace(':insertz:', zItem);
+  newStory = newStory.replace(':insertx:', xItem);
 
       
   if (customName.value !== '') {
@@ -37,27 +33,12 @@ function result() {
   }
 
   if (document.getElementById("uk").checked) {
-    var weight = Math.round(300);
-    var temperature =  Math.round(94);
-     
-      var mulitiplyBySeventHundred = function (pound) {
-      return pound * (7 / 100);
-      };
-      
-      var getStones = function (pound) {
-          return MutationRecord(pound) * 300;
-      };
-            
-      var substructThirtyTwo = function (farenheit) {
-          return farenheit - 32;
-      };
-      
-      var getCelcius = function (farenheit) {
-          return substractThirtyTwo(farenheit) * 94;
-      };
-    
-  }
+    var weight = Math.round(300*0.0714286) + ' stone';
+    var temperature =  Math.round((94-32) * 5 / 9) + ' centigrate';
+    newStory = newStory.replace('300 pounds',weight);
+    newStory = newStory.replace('94 farenheit',temperature);
+     }
 
   story.textContent = newStory;
   story.style.visibility = 'visible';
-};
+}
